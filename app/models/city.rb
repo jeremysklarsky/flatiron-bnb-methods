1961d946328172329593ef347f7913db4972978e
Jeremy Sklarsky <jsklarsky@mediamath.com>
2015-11-12 21:56:10 -0500

    full

--- app/models/city.rb
+++ app/models/city.rb
@@ -8,7 +8,24 @@ class City < ActiveRecord::Base
 
 
   def city_openings(start, finish)
-    openings(start, finish)
+    self.listings.collect do |listing|
+      listing if listing.avail?(start, finish)
+    end
   end
+
+  def res_to_listings_ratio
+    self.listings.size.to_f / self.reservations.size  
+  end
+
+  # class methods
+
+  def self.highest_ratio_res_to_listings
+    City.all.sort_by {|city| city.res_to_listings_ratio}.first
+  end
+
+  def self.most_res
+    City.all.sort_by {|city| city.reservations.size}.last
+  end
+
 end
 
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,11 +2,31 @@ class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
   has_many :reservations, :through => :listings
-  include Mathable::InstanceMethods
-  extend Mathable::ClassMethods
+  # include Mathable::InstanceMethods
+  # extend Mathable::ClassMethods
 
 
   def neighborhood_openings(start, finish)
-    openings(start, finish)
+    self.listings.collect do |listing|
+      listing if listing.avail?(start, finish)
+    end
   end
+
+  def res_to_listings_ratio
+    self.listings.size.to_f / self.reservations.size
+  end
+
+  # class methods
+
+  def self.highest_ratio_res_to_listings
+    Neighborhood.all.select{|hood| hood.reservations.size > 0}
+                .sort_by{|hood|hood.res_to_listings_ratio}
+                .first
+  end
+
+  def self.most_res
+    Neighborhood.all.sort_by {|hood| hood.reservations.size}.last
+  end
+
+
 end
